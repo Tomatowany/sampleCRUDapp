cf82391b88e1b77bc1531aec15b2258d6fd7afaf
Angelo John <146217744+Tomatowany@users.noreply.github.com>
2023-10-31 22:16:59 +0800

    update?

--- functions/add.php
+++ functions/add.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADD DATA</title>
-</head>
-<body>
-    <?php
-        include_once("../dbconnection/mysql-connection.php");
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ADD DATA</title>
+    </head>
+    <body>
+        <?php
+            // including the database connection file
+               include_once("../dbconnection/mysql-connection.php");
 
-        if(isset($_POST['Submit'])) {
-            $code = $_POST['code'];
-            $name =$_POST['name'];
-            // checking empty fields
-
-            if(empty ($code) || empty($name)) {
-                if(empty($name)) {
-                    echo "front color='red'>Subject Name field is empty.</font><br/>";
+            if(isset($_POST['Submit'])) {
+                $code = $_POST['code'];
+                $name =$_POST['name'];
+                // checking empty fields
+                if(empty($code) || empty($name)) {
+                    if(empty($name)) {
+                        echo "<front color='red'>Subject Name field is empty.</font><br/>";
+                    }
+                    if(empty($code)) {
+                        echo "<front color='red'>Subject Code field is empty.</font><br/>";
+                    }
+                    // link to the previous page
+                    echo "<br/><a href='javasacript:self.history.back();'>Go Back</a>";
                 }
-                if(empty($code)) {
-                    echo "front color='red'>Subject Code field is empty.</font><br/>";
-                }
-                //link to previous page
-                echo "<br/><a href='javasacript:self.history.back();'>Go Back</a>";
-            }else{
-                 // if all the fields are filled (not empty)
+                else{
+                    // if all the fields are filled (not empty)
                     // insert data to database
-                $result = mysqli_query($dbc, "INSERT INTO tblsubjects(Subject_Code, Subject_Name) VALUES('$code','$name')");
-                //display message success
-                echo "<font color='green'>Data added successfully.";
-                echo "<br/><a href='../index.php'>View Result</a>";
+                    $result = mysqli_query($dbc, "INSERT INTO tblsubjects(Subject_Code, Subject_Name) VALUES('$code','$name')");
+                    // display success message
+                    echo "<font color='green'>Data added successfully.";
+                    echo "<br/><a href='../index.php'>View Result</a>";
+                }
             }
-        }
-    ?>
-</body>
+        ?>
+    </body>
 </html>
